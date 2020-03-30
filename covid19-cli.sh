8d85d8d21be70d2cea703387bdb1ac9ef302451d
rizkyzhang <41568558+rizkyzhang@users.noreply.github.com>
2020-03-30 14:42:53 +0700

    fix: fix typo

--- covid19-cli.sh
+++ covid19-cli.sh
@@ -8,8 +8,9 @@ API_TOTAL_ENDPOINT="$BASE_API/all"
 function get_data() {
   country=$1
 
-  cases=${curl -s $BASE_API/$country | jq ".cases"}
-  deaths=${curl -s $BASE_API/$country | jq ".deaths"}
-  recovered=${curl -s $BASE_API/$country | jq ".recovered"}
+  cases=$(curl -s $BASE_API/$country | jq ".cases")
+  deaths=$(curl -s $BASE_API/$country | jq ".deaths")
+  recovered=$(curl -s $BASE_API/$country | jq ".recovered")
 }
+
 
