8f2ed5fe2cbe242a889961d131add509d102919b
rizkyzhang <41568558+rizkyzhang@users.noreply.github.com>
2020-03-30 14:54:07 +0700

    feat: add print_result function

--- covid19-cli.sh
+++ covid19-cli.sh
@@ -2,15 +2,27 @@
 
 VERSION="0.0.1"
 
-BASE_API="https://corona.lmao.ninja/"
+BASE_API="https://corona.lmao.ninja"
+API_ALL_COUNTRIES_ENDPOINT="$BASE_API/countries"
 API_TOTAL_ENDPOINT="$BASE_API/all"
 
 function get_data() {
   country=$1
 
-  cases=$(curl -s $BASE_API/$country | jq ".cases")
-  deaths=$(curl -s $BASE_API/$country | jq ".deaths")
-  recovered=$(curl -s $BASE_API/$country | jq ".recovered")
+  echo $API_ALL_COUNTRIES_ENDPOINT
+
+  cases=$(curl -s $API_ALL_COUNTRIES_ENDPOINT/$country | jq ".cases")
+  deaths=$(curl -s $API_ALL_COUNTRIES_ENDPOINT/$country | jq ".deaths")
+  recovered=$(curl -s $API_ALL_COUNTRIES_ENDPOINT/$country | jq ".recovered")
 }
 
+function print_result() {
+  get_data $1
+
+  echo "Cases     : ${cases}"
+  echo "Deaths    : ${deaths}"
+  echo "Recovered : ${recovered}"
+}
+
+print_result $1
 
