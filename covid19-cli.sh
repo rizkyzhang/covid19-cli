175a498dad91fd383649d19343191a5096ed774b
rizkyzhang <41568558+rizkyzhang@users.noreply.github.com>
2020-03-30 14:36:02 +0700

    feat: create get_data function

--- covid19-cli.sh
+++ covid19-cli.sh
@@ -3,5 +3,13 @@
 VERSION="0.0.1"
 
 BASE_API="https://corona.lmao.ninja/"
-API_TOTAL_ENDPOINT="${BASE_API}/all"
+API_TOTAL_ENDPOINT="$BASE_API/all"
+
+function get_data() {
+  country=$1
+
+  cases=${curl -s $BASE_API/$country | jq ".cases"}
+  deaths=${curl -s $BASE_API/$country | jq ".deaths"}
+  recovered=${curl -s $BASE_API/$country | jq ".recovered"}
+}
 
