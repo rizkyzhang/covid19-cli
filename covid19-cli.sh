4810482b006f0512fd664c6ec285dc3569af4ea2
rizkyzhang <41568558+rizkyzhang@users.noreply.github.com>
2020-03-31 10:03:40 +0700

    feat: add option to get world stats

--- covid19-cli.sh
+++ covid19-cli.sh
@@ -1,34 +1,52 @@
 #!/usr/bin/env bash 
 
-VERSION="0.0.1"
+VERSION="0.1.0"
 
 BASE_API="https://corona.lmao.ninja"
 API_ALL_COUNTRIES_ENDPOINT="$BASE_API/countries"
 API_TOTAL_ENDPOINT="$BASE_API/all"
 
-function get_data() {
+function get_single_country_stats() {
   country=$1
 
-  arr=($(curl -s $API_ALL_COUNTRIES_ENDPOINT/$country | jq -r ".cases,.todayCases,.deaths,.todayDeaths,.recovered | @sh"))
-  
-  cases=${arr[0]}
-  today_cases=${arr[1]}
-  deaths=${arr[2]}
-  today_deaths=${arr[3]}
-  recovered=${arr[4]}
+  stats=($(curl -s $API_ALL_COUNTRIES_ENDPOINT/$country | jq -r ".cases,.todayCases,.deaths,.todayDeaths,.recovered | @sh")) 
+
+  cases=${stats[0]}
+  today_cases=${stats[1]}
+  deaths=${stats[2]}
+  today_deaths=${stats[3]}
+  recovered=${stats[4]}
 }
 
-function print_result() {
-  get_data $1
+function get_world_stats() {
+  stats=($(curl -s $API_TOTAL_ENDPOINT | jq -r ".cases,.deaths,.recovered | @sh"))
 
+  cases=${stats[0]}
+  deaths=${stats[1]}
+  recovered=${stats[2]}
+}
+
+function print_stats() {
   RED=$(tput setaf 1)
   GREEN=$(tput setaf 2)
   YELLOW=$(tput setaf 3)
 
-  echo "${YELLOW}Cases     : ${cases} +${today_cases}"
-  echo "${RED}Deaths    : ${deaths} +${today_deaths}"
-  echo "${GREEN}Recovered : ${recovered}"
+  if [[ "$1" == "world" ]]; then
+    get_world_stats
+
+    echo "${YELLOW}Cases     = ${cases}"
+    echo "${RED}Deaths    = ${deaths}"
+    echo "${GREEN}Recovered = ${recovered}"
+
+    exit
+  fi
+
+  get_single_country_stats $1
+
+  echo "${YELLOW}Cases     = ${cases} +${today_cases}"
+  echo "${RED}Deaths    = ${deaths} +${today_deaths}"
+  echo "${GREEN}Recovered = ${recovered}"
 }
 
-print_result $1
+print_stats $1
 
