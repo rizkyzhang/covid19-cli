fc627cc5f51158c15cac337345d592f332a29c30
rizkyzhang <41568558+rizkyzhang@users.noreply.github.com>
2020-04-03 13:16:30 +0700

    feat: input country names with two words

--- covid19-cli.sh
+++ covid19-cli.sh
@@ -4,47 +4,40 @@ VERSION="0.1.0"
 
 API="https://corona-stats.online"
 
-function get_single_country_stats() {
-  country=$1
+function get_stats() {
+  if [ "$1" == "world" ]; then
+    stats=($(curl -s "${API}?format=json" | jq -r ".worldStats | .cases,.todayCases,.deaths,.todayDeaths,.recovered | @sh")) 
+    return $stats
+  fi
+  
+  if [ -n "$1" ] && [ -n "$2" ]; then
+    country="$1%20$2"
+  else
+    country="$1"
+  fi
 
   stats=($(curl -s "${API}/${country}?format=json" | jq -r ".data[] | .cases,.todayCases,.deaths,.todayDeaths,.recovered | @sh"))
+
+  return $stats
+}
+
+function print_stats() {
+  get_stats $@
+
+  RED=$(tput setaf 1)
+  GREEN=$(tput setaf 2)
+  YELLOW=$(tput setaf 3)
 
   cases=${stats[0]}
   today_cases=${stats[1]}
   deaths=${stats[2]}
   today_deaths=${stats[3]}
   recovered=${stats[4]}
-}
-
-function get_world_stats() {
-  stats=($(curl -s "${API}?format=json" | jq -r ".worldStats | .cases,.todayCases,.deaths,.todayDeaths,.recovered | @sh")) 
-
-  cases=${stats[0]}
-  deaths=${stats[1]}
-  recovered=${stats[2]}
-}
-
-function print_stats() {
-  RED=$(tput setaf 1)
-  GREEN=$(tput setaf 2)
-  YELLOW=$(tput setaf 3)
-
-  if [[ "$1" == "world" ]]; then
-    get_world_stats
-
-    echo "${YELLOW}Cases     = ${cases}"
-    echo "${RED}Deaths    = ${deaths}"
-    echo "${GREEN}Recovered = ${recovered}"
-
-    exit
-  fi
-
-  get_single_country_stats $1
 
   echo "${YELLOW}Cases     = ${cases} +${today_cases}"
   echo "${RED}Deaths    = ${deaths} +${today_deaths}"
   echo "${GREEN}Recovered = ${recovered}"
 }
 
-print_stats $1
+print_stats $@
 
